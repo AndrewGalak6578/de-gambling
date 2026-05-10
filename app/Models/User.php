aac9e0493b0985d86ca3e643bf105f8fc98a5ed5
AndrewGalak6578 <andrew.galaktionov@inbox.ru>
2026-05-10 19:33:03 +0500

    feat(models): add user domain relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,16 +6,93 @@ namespace App\Models;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection<int, Wallet> $wallets
+ * @property-read Collection<int, Transaction> $transactions
+ * @property-read Collection<int, Bet> $bets
+ * @property-read Collection<int, RiskEvent> $riskEvents
+ * @property-read Collection<int, Intervention> $interventions
+ * @property-read Collection<int, AdminAction> $adminActions
+ * @property-read Collection<int, AdminAction> $targetedAdminActions
+ */
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    /**
+     * @return HasMany<Wallet, $this>
+     */
+    public function wallets(): HasMany
+    {
+        return $this->hasMany(Wallet::class);
+    }
+
+    /**
+     * @return HasMany<Transaction, $this>
+     */
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    /**
+     * @return HasMany<Bet, $this>
+     */
+    public function bets(): HasMany
+    {
+        return $this->hasMany(Bet::class);
+    }
+
+    /**
+     * @return HasMany<RiskEvent, $this>
+     */
+    public function riskEvents(): HasMany
+    {
+        return $this->hasMany(RiskEvent::class);
+    }
+
+    /**
+     * @return HasMany<Intervention, $this>
+     */
+    public function interventions(): HasMany
+    {
+        return $this->hasMany(Intervention::class);
+    }
+
+    /**
+     * @return HasMany<AdminAction, $this>
+     */
+    public function adminActions(): HasMany
+    {
+        return $this->hasMany(AdminAction::class, 'admin_user_id');
+    }
+
+    /**
+     * @return MorphMany<AdminAction, $this>
+     */
+    public function targetedAdminActions(): MorphMany
+    {
+        return $this->morphMany(AdminAction::class, 'target');
+    }
 
     /**
      * Get the attributes that should be cast.
